13f95a6fd6079f2c78e744dd502a708f553db3a6
lvandam <laurens@vand.am>
2018-03-07 10:24:45 +0100

    Update matlab script with filtering options

--- results/precision_compare/precision_compare.m
+++ results/precision_compare/precision_compare.m
@@ -6,10 +6,11 @@ datalength = length(data{:,1});
 
 % Config (define different regions that should be plotted individually)
 main_title = 'Dataset = 32\_data.txt - Initial constant = 2^{1}';
-regions = [1 datalength; 4543 4638; 7039 7134; 7135 datalength]; % start and end indices of each region
-titles = ["All intermediate values", "M[1], X[1], Y[1]", "M[27], X[27], Y[27]", "Result Accumulation"]; % region labels
-showlabels = [false, true, true, true]; % toggle X-axis variable names for each region
-wide = [true, true, true, true]; % horizontally wide plot for region
+regions = [1 datalength; 4543 4638; 4543 4638; 7039 7134; 7135 datalength]; % start and end indices of each region
+titles = ["All intermediate values", "M[1][c]", "Y[1][c]", "M[27], X[27], Y[27]", "Result Accumulation"]; % region labels
+showlabels = [false, true, true, true, true]; % toggle X-axis variable names for each region
+wide = [true, false, false, true, true]; % horizontally wide plot for region
+filter_text = ["", "M", "Y", "", ""]; % filtered based on the first (name) column
 
 % Process
 wide_count = nnz(wide == true);
@@ -52,14 +53,30 @@ for row = 1:size(regions,1)
     
     % Plot
     hold on
-    scatter(start_idx:end_idx, data{start_idx:end_idx,4}, 30, 'MarkerEdgeColor', 'none', 'MarkerFaceColor', 'b', 'MarkerFaceAlpha', .7) % float
-    scatter(start_idx:end_idx, data{start_idx:end_idx,5}, 30, 'MarkerEdgeColor', 'none', 'MarkerFaceColor', 'r', 'MarkerFaceAlpha', .7) % posit<32,2>
+    
+    % Match with any given filter (start string)
+    if filter_text(row) == ""
+        data_x_filtered = find(ones(datalength,1) >= 0);
+        data_matching = data_x_filtered >= start_idx & data_x_filtered <= end_idx;
+        data_x_filtered = find(data_matching);
+    else
+        data_matching1 = strncmp(data{:,1}, filter_text(row), length(filter_text(row)));
+        data_x_filtered = find(data_matching1 >= 0);
+        data_matching2 = data_x_filtered >= start_idx & data_x_filtered <= end_idx;
+        data_matching = data_matching1 .* data_matching2;
+        data_x_filtered = find(data_matching);
+    end
+    
+    data_float_filtered = data{data_x_filtered, 4}
+    data_posit_filtered = data{data_x_filtered, 5};
+    
+    scatter(data_x_filtered, data_float_filtered, 30, 'MarkerEdgeColor', 'none', 'MarkerFaceColor', 'b', 'MarkerFaceAlpha', .7) % float %data{start_idx:end_idx,4},
+    scatter(data_x_filtered, data_posit_filtered, 30, 'MarkerEdgeColor', 'none', 'MarkerFaceColor', 'r', 'MarkerFaceAlpha', .7) % posit<32,2> %data{start_idx:end_idx,5},
     legend({'float', 'posit<32,2>'});
     grid on; grid minor;
     set(gca, 'TickLabelInterpreter', 'none');
     if showlabels(row)
-        xlabel('name');
-        set(gca, 'xtick', start_idx:end_idx, 'xticklabel', data{start_idx:end_idx,1})
+        set(gca, 'xtick', data_x_filtered, 'xticklabel', data{data_x_filtered,1})
     end
     xtickangle(45);
     title(titles(row)); ylabel('log_{10}(|relative error|)');
