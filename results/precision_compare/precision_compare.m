8822b252958189f8a359ba135f3fd6b435ca6d95
lvandam <laurens@vand.am>
2018-03-15 11:33:40 +0100

    Add results

--- results/precision_compare/precision_compare.m
+++ results/precision_compare/precision_compare.m
@@ -14,29 +14,29 @@ for initial_constant = initial_constants
     % Config (define different regions that should be plotted individually)
 
     % 32_data
-    set_name = '32_data';
-    main_title = ['Dataset = ' strrep(set_name, '_', '\_') ' - Initial constant = 2^{' num2str(initial_constant) '}' newline 'git commit ' git_hash_string ' / ' git_branch_name];
-    pngname = set_name+"_"+git_hash_string+"-"+git_branch_name+"_const"+num2str(initial_constant);
-    regions = [1 datalength; 1 datalength; 1 datalength; 1 datalength; 1 datalength; 1 datalength]; % start and end indices of each region
-    titles = ["All intermediate values", "Phred scores", "M[1][c]", "Y[1][c]", "M[27], X[27], Y[27]", "Result Accumulation"]; % region labels
-    showlabels = [false, true, true, true, true, true]; % toggle X-axis variable names for each region
-    wide = [true, true, false, false, true, true]; % horizontally wide plot for region
-    
-    filter_text1 = ["",     "p",    "M[1]", "Y[1]", "M[27]", "result"]; % filtered based on the first (name) column
-    filter_text2 = ["NONE", "NONE", "NONE", "NONE", "X[27]", "NONE"];
-    filter_text3 = ["NONE", "NONE", "NONE", "NONE", "Y[27]", "NONE"];
-
-    % 1_data
-%     set_name = '1_data';
+%     set_name = '32_data';
 %     main_title = ['Dataset = ' strrep(set_name, '_', '\_') ' - Initial constant = 2^{' num2str(initial_constant) '}' newline 'git commit ' git_hash_string ' / ' git_branch_name];
 %     pngname = set_name+"_"+git_hash_string+"-"+git_branch_name+"_const"+num2str(initial_constant);
-%     regions = [1 datalength]; % start and end indices of each region
-%     titles = ["All intermediate values"]; % region labels
-%     showlabels = [true]; % toggle X-axis variable names for each region
-%     wide = [true]; % horizontally wide plot for region
-%     filter_text1 = [""]; % filtered based on the first (name) column
-%     filter_text2 = ["NONE"];
-%     filter_text3 = ["NONE"];
+%     regions = [1 datalength; 1 datalength; 1 datalength; 1 datalength; 1 datalength; 1 datalength]; % start and end indices of each region
+%     titles = ["All intermediate values", "Phred scores", "M[1][c]", "Y[1][c]", "M[27], X[27], Y[27]", "Result Accumulation"]; % region labels
+%     showlabels = [false, true, true, true, true, true]; % toggle X-axis variable names for each region
+%     wide = [true, true, false, false, true, true]; % horizontally wide plot for region
+%     
+%     filter_text1 = ["",     "p",    "M[1]", "Y[1]", "M[27]", "result"]; % filtered based on the first (name) column
+%     filter_text2 = ["NONE", "NONE", "NONE", "NONE", "X[27]", "NONE"];
+%     filter_text3 = ["NONE", "NONE", "NONE", "NONE", "Y[27]", "NONE"];
+
+    % 1_data
+    set_name = '1_data';
+    main_title = ['Dataset = ' strrep(set_name, '_', '\_') ' - Initial constant = 2^{' num2str(initial_constant) '}' newline 'git commit ' git_hash_string ' / ' git_branch_name];
+    pngname = set_name+"_"+git_hash_string+"-"+git_branch_name+"_const"+num2str(initial_constant);
+    regions = [1 datalength]; % start and end indices of each region
+    titles = ["All intermediate values"]; % region labels
+    showlabels = [true]; % toggle X-axis variable names for each region
+    wide = [true]; % horizontally wide plot for region
+    filter_text1 = [""]; % filtered based on the first (name) column
+    filter_text2 = ["NONE"];
+    filter_text3 = ["NONE"];
 
     % Process
     wide_count = nnz(wide == true);
@@ -118,7 +118,7 @@ for initial_constant = initial_constants
     p = mtit(main_title, 'FontSize', 14, 'Color', 'r', 'XOff', 0, 'YOff', .025);
     
     % Save as PNG
-    print(pngname, '-dpng');
+    print('images\'+pngname, '-dpng');
 end
 
 close;
