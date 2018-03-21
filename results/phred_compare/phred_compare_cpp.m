b41395e49f8a99d667d9d886bc9351cd4208c31f
lvandam <laurens@vand.am>
2018-03-21 15:53:49 +0100

    Updated Phred score benchmark

--- results/phred_compare/phred_compare_cpp.m
+++ results/phred_compare/phred_compare_cpp.m
@@ -1,17 +1,36 @@
 clf;
 
 data = csvread('phred_float_posit.txt', 1);
+pdfname = 'phred_float_posit';
 
 Q = data(:,1);
-dE_f = data(:,2);
-dE_f_log = data(:,3);
-dE_p = data(:,4);
-dE_p_log = data(:,5);
+da_f = data(:,2);
+da_p2 = data(:,3);
+da_p3 = data(:,4);
 
-scatter(Q, dE_f_log, 'r');
+clf; h = figure;
+set(h, 'units', 'pixels', 'position', [200, 200, 600, 200]);
+
 hold on;
-scatter(Q, dE_p_log, 'b');
+scatter(Q, da_f, 'r');
+scatter(Q, da_p2, 'b');
+scatter(Q, da_p3, 'g');
 xlabel('Phred Quality Score');
-ylabel('log_{10}(relative error)');
+ylabel('decimal accuracy');
 grid on; grid minor;
-legend({'float32','posit<32,2>'}, 'Location', 'northwest');
+legend({'float','posit<32,2>','posit<32,3>'}, 'Location', 'northeast');
+
+% Save as PDF
+% Tight boundaries
+ax = gca;
+outerpos = ax.OuterPosition;
+ti = ax.TightInset; 
+left = outerpos(1) + ti(1);
+bottom = outerpos(2) + ti(2);
+ax_width = outerpos(3) - ti(1) - ti(3);
+ax_height = outerpos(4) - ti(2) - ti(4);
+ax.Position = [left bottom ax_width ax_height];
+
+pos = get(h, 'Position');
+set(h, 'PaperPositionMode', 'Auto', 'PaperUnits', 'points ', 'PaperSize', [pos(3), pos(4)])
+print(h, pdfname, '-dpdf', '-r0')
